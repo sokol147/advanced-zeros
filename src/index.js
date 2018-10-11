175338f18c7e2a7e08648c42957b68e02e0b9b80
Sokolov V <sokolov.stb@gmail.com>
2018-10-11 10:58:40 +0300

    refactor: remove trash

--- src/index.js
+++ src/index.js
@@ -1,46 +1,29 @@
 module.exports = function getZerosCount(number, base) {
-  let resultForTwo = 0, resultForLast = 0, num = 2;
-  const factors = [];
+  let tempBase = base;
+  let maxNumber = Number.MAX_VALUE;
 
-  function getSimpleNum(base){
-    if(base === 1){
-      factors.push(1);
-    }
-    if(base % num === 0){
-      factors.push(num);
-      base = base / num;
-      getSimpleNum(base);
-    } else if(base % num !== 0){
-      if(!(base <= num)){
-        num++;
-        getSimpleNum(base);
+  for(let i = 2; i <= tempBase; i++){
+    if(tempBase % i === 0){
+
+      let denominator = 0;
+
+      while(tempBase % i === 0){
+        tempBase /= i;
+        denominator++;
       }
+
+      let innerRes = Math.floor(number / i);
+
+      let count = 0;
+
+      while(innerRes > 0){
+        count += innerRes;
+        innerRes = Math.floor(innerRes / i);
+      }
+
+      maxNumber = Math.min(maxNumber, Math.floor(count / denominator));
     }
   }
-  getSimpleNum(base);
 
-  factors.sort(function(a,b) {return a - b});
-  if(factors[0] == 1) factors.shift();
-
-  let multiplyForFirst = 0, multiplyForLast = 0;
-
-  for(let i = 0, len = factors.length; i < len; i++){
-    if(factors[0] == factors[i]) multiplyForFirst++;
-    if(factors[factors.length - 1] == factors[i]) multiplyForLast++;
-  }
-
-  for(let i = 1, len = number; i < len; i++){
-    resultForTwo += Math.floor(number / Math.pow(factors[0],i));
-    resultForLast += Math.floor(number / Math.pow(factors[factors.length - 1],i));
-    if(Math.floor(number / Math.pow(2,i)) == 0 ||
-       Math.floor(number / Math.pow(factors[factors.length - 1],i)) == 0) break;
-  }
-  let resultForBaseTwo = Math.floor(resultForTwo / multiplyForFirst);
-  let resultForBaseLast = Math.floor(resultForLast / multiplyForLast);
-
-  if(multiplyForLast == 1){
-    return resultForBaseLast;
-  } else {
-    return Math.min(resultForBaseTwo, resultForBaseLast);
-  }
+  return maxNumber;
 }
